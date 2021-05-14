53a0df1ab215540c8d491efc0513f7c162f3f1af
mickey li <mickeyhli@outlook.com>
2021-05-14 20:23:38 +0100

    Added ws281x testing library

---
+++ ws281x_test/setup.py
@@ -0,0 +1,28 @@
+from glob import glob
+from setuptools import setup
+
+package_name = 'ws281x_test'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/'+ package_name, glob('launch/*.launch*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'test = ws281x_test.test:main'
+        ],
+    },
+)
---
+++ ws281x_test/ws281x_test/test.py
@@ -0,0 +1,53 @@
+import rclpy
+from rclpy.node import Node
+
+import time
+
+from led_msgs.srv import SetLEDs
+from led_msgs.msg import LEDStateArray, LEDState
+
+class WS281XTest(Node):
+
+    def __init__(self):
+        super().__init__('ws281x_test')
+
+        self.led_count = self.get_parameter_or('led_count', 30)
+        self.set_leds = self.create_client(SetLEDs, 'set_leds')
+
+    def fill_strip(self, red, green, blue):
+        req = SetLEDs()
+        req.leds = [LEDState(i, red, green, blue) for i in range(self.led_count)]
+        return self.set_leds.call_async(req) # returns a future object
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    wtest = WS281XTest()
+
+    fut = wtest.fill_strip(200, 200, 200)
+    rclpy.spin_until_future_complete(wtest, fut)
+    time.sleep(1)
+    
+
+    fut = wtest.fill_strip(0, 200, 0)
+    rclpy.spin_until_future_complete(wtest, fut)
+    time.sleep(1)
+
+    fut = wtest.fill_strip(0, 0, 200)
+    rclpy.spin_until_future_complete(wtest, fut)
+    time.sleep(1)
+
+    fut = wtest.fill_strip(200, 0, 0)
+    rclpy.spin_until_future_complete(wtest, fut)
+    time.sleep(1)
+
+    fut = wtest.fill_strip(0, 0, 0)
+    rclpy.spin_until_future_complete(wtest, fut)
+
+    wtest.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+
