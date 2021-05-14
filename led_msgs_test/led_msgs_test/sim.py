009e74996b3004dab66c6aba00625ba15792b7a4
Mickey Li <mickeyhli@outlook.com>
2021-05-14 11:54:08 +0100

    Update sim.py

--- led_msgs_test/led_msgs_test/sim.py
+++ led_msgs_test/led_msgs_test/sim.py
@@ -17,7 +17,8 @@ class LEDController(Node):
         self.led_count = self.get_parameter_or('~led_count', 30)
         self.state_pub = self.create_publisher(LEDStateArray, 'state', 10)
 
-        self.state = LEDStateArray([LEDState(index=index) for index in range(self.led_count)])
+        self.state = LEDStateArray()
+        self.state.leds = [LEDState(index=index) for index in range(self.led_count)]
 
         self.set_led_srv = self.create_service(SetLED, 'set_led', self.set_led)
         self.set_leds_srv = self.create_service(SetLEDs, 'set_leds', self.set_leds)
